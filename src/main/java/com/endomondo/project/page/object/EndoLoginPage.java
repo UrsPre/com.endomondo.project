ca59cae4ab15c866640699b906a2e269c23a34d9
Ula Predki <urszula.predki@lumesse.com>
2018-05-28 15:18:05 +0200

    zle

--- src/main/java/com/endomondo/project/page/object/EndoLoginPage.java
+++ src/main/java/com/endomondo/project/page/object/EndoLoginPage.java
@@ -2,19 +2,30 @@ package com.endomondo.project.page.object;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class EndoLoginPage {
     //page elements are defined
 
     private WebElement email;
     private WebElement password;
-    
+
     @FindBy(xpath = "//span[@class='ng-binding ng-scope']")
     private WebElement loginButton;
 
+    @FindBy(className = "MonthTrialPopup-logo")
+    private List<WebElement> popup;
+
+    @FindBy(className = "MonthTrialPopup-close")
+    private WebElement popupCloseButton;
+
+    @FindBy(xpath = "//img[@alt='Endomondo Logo']")
+    private WebElement endomondoLogo;
 
     private WebDriver driver;
     //creating constructor
@@ -29,4 +40,14 @@ public class EndoLoginPage {
     public void setPassword(String password_input) { password.sendKeys(password_input); }
 
     public void loginToAccount() { loginButton.click(); }
+
+    public void closePopupIfDisplayed() {
+        if(popup.size() >= 1) {
+            popupCloseButton.click();
+        }
+    }
+
+    public WebElement getLogo() {
+        return endomondoLogo;
+    }
 }
--- src/test/java/com/endomondo/project/test/EndoLoginPageTest.java
+++ src/test/java/com/endomondo/project/test/EndoLoginPageTest.java
@@ -1,19 +1,28 @@
 package com.endomondo.project.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import com.endomondo.project.page.object.EndoLoginPage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class EndoLoginPageTest extends EndoSuiteTest {
 
     @Test
     public void EndoLoginPageTest(){
-        //creating a copy of the page class EndoLoginPage
-        //and passing the driver object
-        EndoLoginPage endoLogin = new EndoLoginPage(driver);
 
-        //set email
-        endoLogin.setEmail("majmagda1@o2.pl");
-        endoLogin.setPassword("ThePassword1");
-        endoLogin.loginToAccount();
+        //given
+        EndoLoginPage endoLogin = new EndoLoginPage(driver); //creating a copy of the page class EndoLoginPage and passing the driver object
+        endoLogin.setEmail("majmagda1@o2.pl"); //set email
+        endoLogin.setPassword("ThePassword1"); //set password
+        endoLogin.loginToAccount(); //clicking button
+        endoLogin.closePopupIfDisplayed();
+        //when
+        //then
+
+        assertTrue(endoLogin.getLogo().isDisplayed());
     }
+
 }
